20ec9d84cb07b37f6f72f4ad114d80a20f9c3196
Ayaana <ayanka_m@mail.ru>
2020-07-09 19:10:44 +0300

    2

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-let cvs = document.getElementById("canvas");//настройка канваса
+let cvs = document.getElementById("canvas1");//настройка канваса
 let ctx = cvs.getContext("2d");
 
 let img1 = new Image();
-let img2 = new Image();
-let img3 = new Image();
-let img4 = new Image();
-let img5 = new Image();
 
-img1.src = "img/1.jpg"; //путь к изображениям
-img2.src = "img/2.jpg";
-img3.src = "img/3.jpg";
-img4.src = "img/4.jpg";
-img5.src = "img/5.jpg";
+img1.src = "img/1.jpg";
 
-ctx.beginPath(); //заливка фона
-ctx.rect(60, 10, 1200, 600);
-ctx.fillStyle = "#FFFAC8";
-ctx.fill();
+let part_img1 = new Image(); //части изображений
+let part_img2 = new Image();
+let part_img3 = new Image();
+let part_img4 = new Image();
+let part_img5 = new Image();
+let part_img6 = new Image();
+let part_img7 = new Image();
+let part_img8 = new Image();
+let part_img9 = new Image();
+let part_img10 = new Image();
+let part_img11 = new Image();
 
-let x = 60;
-let y = 20;
+part_img1.src = "img/image_part_001.jpg"; //путь к изо
+part_img2.src = "img/image_part_002.jpg";
+part_img3.src = "img/image_part_003.jpg";
+part_img4.src = "img/image_part_004.jpg";
+part_img5.src = "img/image_part_005.jpg";
+part_img6.src = "img/image_part_006.jpg";
+part_img7.src = "img/image_part_007.jpg";
+part_img8.src = "img/image_part_008.jpg";
+part_img9.src = "img/image_part_009.jpg";
+part_img10.src = "img/image_part_010.jpg";
+part_img11.src = "img/image_part_011.jpg";
 
-function draw() { //начальное положение картинок
-  for(let i=0; i<20; i++) {
-    x += 20;
-    y += 20;
-    //ctx.drawImage(img1, x+i, y+i, 100, 180);
-  }
-  //ctx.drawImage(img1, x, y, 100, 180);
-  ctx.drawImage(img2, 400, 400, 100, 180);
-  ctx.drawImage(img3, 1000, 20, 100, 180);
-  ctx.drawImage(img4, 1100, 420, 100, 180);
-  ctx.drawImage(img5, 520, 300, 100, 180);
+
+function draw() {
+  ctx.drawImage(img1, 0, 0);
 }
 
+img1.onload = draw;
 
-img5.onload = draw;
+document.addEventListener("click", putUp);
+
+function putUp() {
+  //alert("Начинайте собирать!");
+  ctx.beginPath(); //заливка фона новый квадрат
+  ctx.rect(0, 0, 1000, 600);
+  ctx.fillStyle = "#FFFAC8";
+  ctx.fill();
+
+  ctx.drawImage(part_img11, 0, 0);//написать рандом
+  ctx.drawImage(part_img5, 250, 0);
+  ctx.drawImage(part_img2, 500, 0);
+  ctx.drawImage(part_img8, 750, 0);
+  ctx.drawImage(part_img1, 0, 200);
+  ctx.drawImage(part_img9, 250, 200);
+  ctx.drawImage(part_img6, 500, 200);
+  ctx.drawImage(part_img3, 750, 200);
+  ctx.drawImage(part_img10, 0, 400);
+  ctx.drawImage(part_img3, 250, 400);
+  ctx.drawImage(part_img7, 500, 400);
+}
